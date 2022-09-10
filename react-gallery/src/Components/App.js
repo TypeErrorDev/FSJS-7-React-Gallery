c517fd424ff4a3d2eef7b717e716bb1b111942c2
Matthew Pantel <79867801+TypeErrorDev@users.noreply.github.com>
2022-09-10 19:42:49 +0300

    ADD: created API function, no errors...RESTORE PPOITN

--- react-gallery/src/Components/App.js
+++ react-gallery/src/Components/App.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 import SearchForm from "./SearchForm";
 import Nav from "./Nav";
 import PhotoContainer from "./PhotoContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <SearchForm />
-      <Nav />
-      <PhotoContainer />
-    </div>
-  )
-}
+let apiKey = "37ec92b1b2e7bb234eb55808fbb5c687";
 
-export default App;
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      photos: [],
+    };
+  }
+
+  performSearch = (query) => {
+    axios
+      .get(
+        `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
+      )
+      .this((response) => {
+        this.setState({ photos: response.data.photos.photo });
+        console.log(response.data.photos.photo);
+      })
+      .catch((error) => {
+        console.log("Error fetching and parsing data", error);
+      });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <SearchForm onSearch={this.performSearch} />
+        <Nav />
+        <PhotoContainer data={this.state.photos} />
+      </div>
+    );
+  }
+}
