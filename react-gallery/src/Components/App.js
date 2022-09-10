a5f5b66de2487da76b60aa9c421088d2286b75f2
TypeErrorDev <matthew.pantel86@gmail.com>
2022-09-10 03:12:38 +0200

    Created component files, set up the default view, need to work on the axios

--- react-gallery/src/Components/App.js
+++ react-gallery/src/Components/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import SearchForm from "./SearchForm";
+import Nav from "./Nav";
+import PhotoContainer from "./PhotoContainer";
+
 
 function App() {
   return (
     <div className="App">
-
+      <SearchForm />
+      <Nav />
+      <PhotoContainer />
     </div>
   )
 }
