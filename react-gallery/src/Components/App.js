b4da43ed81980f0c2720c93a8772c56e0dffcaea
TypeErrorDev <matthew.pantel86@gmail.com>
2022-09-10 03:34:37 +0200

    cleared white space

--- react-gallery/src/Components/App.js
+++ react-gallery/src/Components/App.js
@@ -3,7 +3,6 @@ import SearchForm from "./SearchForm";
 import Nav from "./Nav";
 import PhotoContainer from "./PhotoContainer";
 
-
 function App() {
   return (
     <div className="App">
--- react-gallery/src/Components/Nav.js
+++ react-gallery/src/Components/Nav.js
@@ -1,14 +1,17 @@
 import React from 'react'
+// import {NavLink} from "react-router-dom";
 
 const Nav = () => {
   return(
     <nav className="main-nav">
       <ul>
-        <li><a href="#">Cats</a></li>
-        <li><a href="#">Dogs</a></li>
-        <li><a href="#">Computers</a></li>
+        <li><a href="/">Cats</a></li>
+        <li><a href="/">Dogs</a></li>
+        <li><a href="/">Computers</a></li>
       </ul>
     </nav>
+
+  // { WRITE THE NAV BUTTON ROUTES HERE }
   )
 }
 
