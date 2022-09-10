d4f03cffbf13c3633c8193b19407471406c3452d
Matthew Pantel <79867801+TypeErrorDev@users.noreply.github.com>
2022-09-10 23:05:42 +0300

    ADD: state and some console logs

--- react-gallery/src/Components/App.js
+++ react-gallery/src/Components/App.js
@@ -4,31 +4,41 @@ import SearchForm from "./SearchForm";
 import Nav from "./Nav";
 import PhotoContainer from "./PhotoContainer";
 
-let apiKey = "37ec92b1b2e7bb234eb55808fbb5c687";
-
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
       photos: [],
+      loading: true,
     };
+    console.log(
+      "DEBUG: App -> constructor",
+      " | State: ",
+      this.state.photos.photos
+    );
   }
 
   performSearch = (query) => {
+    // let apiKey = "37ec92b1b2e7bb234eb55808fbb5c687";
     axios
       .get(
-        `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
+        `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=37ec92b1b2e7bb234eb55808fbb5c687&tags=${query}&per_page=24&format=json&nojsoncallback=1`
       )
       .this((response) => {
         this.setState({ photos: response.data.photos.photo });
-        console.log(response.data.photos.photo);
       })
       .catch((error) => {
         console.log("Error fetching and parsing data", error);
       });
+    console.log(
+      "DEBUG: App.js -> axios.get",
+      " | response: ",
+      this.state.photos
+    );
   };
 
   render() {
+    console.log("DEBUG: App -> render", " | api: ", this.state.photos);
     return (
       <div className="container">
         <SearchForm onSearch={this.performSearch} />
